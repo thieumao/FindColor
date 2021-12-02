b85c555acf41b80e3ddce35e85f0b7223ff98640
Thieu Mao <thieumao@gmail.com>
2021-12-03 01:06:38 +0700

    Remove unnecessary code

--- App.tsx
+++ App.tsx
@@ -1,43 +1,7 @@
-// import React, { Component, Fragment } from 'react';
-// import { StatusBar } from 'react-native';
-// // import { Font } from 'expo';
-// // import Routes from './screens/Routes'
-// import Home from "./screens/Home";
-
-// class App extends Component {
-//   state = {
-//     isFontLoaded: false
-//   }
-
-//   async componentDidMount() {
-//     await Font.loadAsync({
-//       'dogbyte': require('./assets/fonts/dogbyte.otf'),
-//     });
-//     this.setState({ isFontLoaded: true })
-//   }
-
-//   render() {
-//     return(<Home />);
-//     // if(!this.state.isFontLoaded) {
-//     //   return <Fragment />
-//     // } else {
-//     //   return (
-//     //     <Fragment>
-//     //       <StatusBar barStyle="light-content" />
-//     //       <Routes />
-//     //     </Fragment>
-//     //   )
-//     // }
-//   }
-// }
-
-// export default App;
-
 import React, { FC } from 'react';
 import { Text } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
-// import Home from "./screens/Home";
 import MainStack from './screens/MainStack'
 import { NavigationContainer } from '@react-navigation/native';
 
--- screens/MainStack.js
+++ screens/MainStack.js
@@ -1,27 +1,3 @@
-// import { createStackNavigator, createAppContainer } from "react-navigation";
-// import Home from "./Home";
-// import Game from "./Game";
-
-// const StackNavigator = createStackNavigator(
-//   {
-//     Home: {
-//       screen: Home
-//     },
-//     Game: {
-//       screen: Game,
-//       navigationOptions: {
-//         gesturesEnabled: false,
-//       },
-//     }
-//   },
-//   {
-//     initialRouteName: "Home",
-//     headerMode: "none",
-//   }
-// );
-
-// export default createAppContainer(StackNavigator);
-
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home';
